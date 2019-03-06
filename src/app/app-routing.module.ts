ec18d755c9280222442bdcdeed4e88e47374a6dd
MarcServat <marcservat@hotmail.com>
2019-03-06 18:49:12 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,8 @@
+import {Container} from 'container/container.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: Container },
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path: 'detail/:id', component: HeroDetailComponent },
+  // { path: 'heroes', component: HeroesComponent }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
+import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent,
+    SearchComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
